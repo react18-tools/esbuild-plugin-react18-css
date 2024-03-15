d422360012dbbeb08b6cc1d5cd514cd47afe5f98
Mayank <mayank.srmu@gmail.com>
2024-03-15 16:32:55 +0530

    Initial commit

---
+++ packages/shared-ui/src/examples/index.tsx
@@ -0,0 +1,10 @@
+import styles from "./examples.module.css";
+import Featured from "./featured";
+
+export default function Examples() {
+  return (
+    <div className={styles.examples}>
+      <Featured />
+    </div>
+  );
+}
