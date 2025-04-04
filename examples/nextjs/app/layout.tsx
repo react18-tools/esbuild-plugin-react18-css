376577b5707d36be76add833d9b77d1665baee27
Mayank <mayank.srmu@gmail.com>
2025-04-04 11:58:42 +0530

    fix build

--- examples/nextjs/app/layout.tsx
+++ examples/nextjs/app/layout.tsx
@@ -1,4 +1,3 @@
-import { NextJsSSGThemeSwitcher } from "nextjs-themes/server/nextjs";
 import { Inter } from "next/font/google";
 import { SharedRootLayout } from "shared-ui";
 
@@ -8,7 +7,6 @@ export default function RootLayout({ children }: { children: React.ReactNode }):
   return (
     <html lang="en">
       <body>
-        <NextJsSSGThemeSwitcher />
         <SharedRootLayout className={inter.className}>{children}</SharedRootLayout>
       </body>
     </html>
